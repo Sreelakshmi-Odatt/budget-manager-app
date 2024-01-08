e7898865179ec6310a2dbef0f361323f2519199e
Sreelakshmi-Odatt <sreelakshmiodatt@gmail.com>
2024-01-07 23:56:51 -0500

    Backend changes
    minor changes made

--- backend/app/models/user/user.py
+++ backend/app/models/user/user.py
@@ -1,19 +1,3 @@
-# from sqlalchemy import Column, String, Integer
-#
-# from sqlalchemy.ext.declarative import declarative_base
-#
-# Base = declarative_base()
-#
-#
-# class User(Base):
-#     __tablename__ = "User"
-#
-#
-# user_id = Column(Integer, primary_key=True)
-# username = Column(String, unique=True)
-# password_hash = Column(String)
-# email = Column(String, unique=True)
-#
 
 
 from sqlalchemy import Column, Integer, String
